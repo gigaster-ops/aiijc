dd0f1279a1aec7000c086f973a8b452a1cc4004f
Кирилл Пантелеев <kiryushka.panteleev.03@mail.ru>
2021-10-16 21:35:48 +0300

    first commit

---
+++ download_model.py
@@ -0,0 +1,5 @@
+from google_drive_downloader import GoogleDriveDownloader as gdd
+
+gdd.download_file_from_google_drive(file_id='1-e-Ni-38UIadxSEA0xWIv0VsDVdqIuAu',
+                                    dest_path='./weights/yolov5l.54.pt',
+                                    unzip=False)
